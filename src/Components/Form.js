f96f860124cf571d13670f45242a9cbc0880c922
vineetkrdixit <vineetdixit.vns@gmail.com>
2022-05-06 01:57:07 +0530

    Data is readed WIP:-making a get req

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,7 +1,39 @@
 import React from "react";
 import "../Components/Form.css";
+import { useState } from "react";
 
 export default function Form() {
+  const [id, setid] = useState("");
+  const [name, setName] = useState("");
+  const [marks, setMarks] = useState("");
+  const [detail, setDetails] = useState([]);
+  const handleChange1 = (event) => {
+    setid(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleChange2 = (event) => {
+    setName(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleChange3 = (event) => {
+    setMarks(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleClick = (event) => {
+    event.preventDefault();
+    const details = {
+      ids: id,
+      names: name,
+      mark: marks,
+    };
+    setDetails([...detail, details]);
+    setid("");
+    setName("");
+    setMarks("");
+    // console.log(details);
+    console.log(detail);
+  };
+
   return (
     <>
       <div className="full-body">
@@ -10,15 +42,38 @@ export default function Form() {
         </div>
         <div className="main-body">
           <div className="col-sm-6 form-wrapper">
-            <form className="form-body">
+            <form className="form-body" action="/submitted" method="GET">
               <label htmlFor="ID">ID</label>
-              <input type="text" className="registrationID" id="ID" />
+              <input
+                type="text"
+                className="registrationID"
+                id="ID"
+                onChange={handleChange1}
+                value={id}
+              />
               <label htmlFor="personname">Name</label>
-              <input type="text" className="name" id="personname" />
+              <input
+                type="text"
+                className="name"
+                id="personname"
+                onChange={handleChange2}
+                value={name}
+              />
               <label htmlFor="totalmarks">Marks</label>
-              <input type="number" className="marks" id="totalmarks" />
+              <input
+                type="number"
+                className="marks"
+                id="totalmarks"
+                onChange={handleChange3}
+                value={marks}
+              />
               <br />
-              <input class="btn btn-success" type="submit" value="Submit" />
+              <input
+                className="btn btn-success"
+                type="submit"
+                value="Submit"
+                onClick={handleClick}
+              />
             </form>
           </div>
           <div className="col-sm-6"></div>
